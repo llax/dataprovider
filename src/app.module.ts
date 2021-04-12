eabe3febfa674f424061b687cad99fde6e20041f
edelwud <edelwud@icloud.com>
2021-04-13 02:21:44 +0300

    add: card module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { FilmModule } from './film/film.module';
 import { TvShowModule } from './tv-show/tv-show.module';
+import { CardModule } from './card/card.module';
 
 @Module({
-  imports: [UserModule, AuthModule, FilmModule, TvShowModule],
+  imports: [UserModule, AuthModule, FilmModule, TvShowModule, CardModule],
   controllers: [],
   providers: [],
 })
