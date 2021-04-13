83762c0d5fd968b7fd5e34333a65d8e9e2f7d429
edelwud <edelwud@icloud.com>
2021-04-13 03:10:51 +0300

    add: config service & user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { FilmModule } from './film/film.module';
 import { TvShowModule } from './tv-show/tv-show.module';
 import { CardModule } from './card/card.module';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [UserModule, AuthModule, FilmModule, TvShowModule, CardModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    FilmModule,
+    TvShowModule,
+    CardModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configService } from './config/config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen((process.env.PORT as unknown) as number | 3000);
+
+  if (!configService.isProduction()) {
+    const document = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Netflex API')
+        .setDescription('Dataprovider')
+        .build(),
+    );
+
+    SwaggerModule.setup('docs', app, document);
+  }
+  await app.listen(3000);
 }
 bootstrap();
